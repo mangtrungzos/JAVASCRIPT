9909706441e124cfef6793e0db080f75b97d2611
mangtrungzos <sangv906@gmail.com>
2024-08-04 18:22:33 +0700

    8/4

---
+++ Array/ArrayOfObject.js
@@ -0,0 +1,30 @@
+const fruits = [{name: "apple", color: "red", calories: 95}, 
+                {name: "orange", color: "orange", calories: 45}, 
+                {name: "banana", color: "yellow", calories: 105}, 
+                {name: "coconut", color: "white", calories: 59}, 
+                {name: "pineapple", color: "yellow", calories: 37}
+];
+
+
+// console.log(fruits[0].name);
+
+// fruits.push({name: "grapes", color: "purple", calories:"62"});
+
+// fruits.splice(1, 2);
+
+// ----------- forEach() --------------
+// fruits.forEach(fruit => console.log(fruit.name));
+
+// ----------- Map() --------------
+// const fruitNames = fruits.map(fruit => fruit.name);
+// console.log(fruitNames);
+
+// ----------- filter() --------------
+// const yellowFruits = fruits.filter(fruit => fruit.color === "yellow");
+// console.log(yellowFruits);
+
+// ----------- reduce() --------------
+// const maxFruit = fruits.reduce((max, fruit) => fruit.calories > max.calories ? fruit : max);
+// console.log(maxFruit);
+
+// console.log(fruits);
