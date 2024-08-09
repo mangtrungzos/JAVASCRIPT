372f14d4a36508ba506ebe48c07119a2f0eaef11
Johan - UTer <101387957+mangtrungzos@users.noreply.github.com>
2024-08-09 14:20:13 +0000

    9/8

--- Closures/index.js
+++ Closures/index.js
@@ -6,9 +6,61 @@
 
 function outer() {
   let message = "hello";
+
+  // func defined inside of outer func
   function inner() {
     console.log(message);
   }
+  inner();
 }
 
 outer();
+
+// State maintenance:
+function createCounter() {
+  let count = 0;
+
+  function increment() {
+    count++;
+    console.log(`Count increased to ${count}`);
+  }
+
+  function getCount() {
+    return count;
+  }
+
+  return { increment, getCount };
+}
+
+const counter = createCounter();
+counter.increment();
+counter.increment();
+counter.increment();
+
+console.log(`the current count is ${counter.getCount()}`);
+
+//
+function createGame() {
+  let score = 0;
+  function increaseScore(points) {
+    score += points;
+    console.log(`+${points}pts`);
+  }
+
+  function decreaseScore(points) {
+    score -= points;
+    console.log(`-${points}pts`);
+  }
+
+  function getScore() {
+    return score;
+  }
+
+  return { increaseScore, decreaseScore, getScore };
+}
+
+const game = createGame();
+
+game.increaseScore(5);
+game.decreaseScore(3);
+console.log(`The final score is ${game.getScore()}pts`);
