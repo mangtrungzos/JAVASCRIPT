a8b4626f6cad02ac5532a210ea988726efb0a073
Johan - UTer <101387957+mangtrungzos@users.noreply.github.com>
2024-08-13 06:19:15 +0000

    13/8

---
+++ CalculatorProgram/index.js
@@ -0,0 +1,20 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+    if (display.value === "error") {
+        display.value = "";
+    }
+    display.value += input;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "error";
+    }
+}
