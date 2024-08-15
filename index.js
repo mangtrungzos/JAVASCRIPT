20939876a35c1483c5cea531091bdb5586f5d142
Johan - UTer <101387957+mangtrungzos@users.noreply.github.com>
2024-08-15 10:34:13 +0000

    15/8

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
-// Error = An Object that is created to represent a problem that occurs
-//         Occur often with user input or estabishing a connection
+function createCounter() {
+  let count = 0;
 
-//         (SyntaxError, TypeError, RangeError, ReferenceError, etc.)
+  function increment() {
+    count++;
+    console.log(`Count increased to ${count}`);
+  }
 
-// try { } = Encloses code that might potentially cause an error
-// catch { } = Catch and handle any thrown Erros from try { }
-// finally { } = (optional) Always executed. Used mostly for clean up
-//               ex. close files, close connections, release resources
+  function getCount() {
+    return count;
+  }
 
-try {
-  const dividend = Number(window.prompt("Enter a dividend: "));
-  const divisor = Number(window.prompt("Enter a divisor: "));
-
-  if (divisor == 0) throw new Error("You can't divide by zero");
-
-  if (isNaN(dividend) || isNaN(divisor))
-    throw new Error("Values must be a number");
-
-  const result = dividend / divisor;
-  console.log(result);
-} catch (err) {
-  console.error(err);
+  return { increment, getCount };
 }
 
-console.log("You have reached the end");
+const counter = createCounter();
+counter.increment();
+counter.increment();
+counter.increment();
+
+console.log(`the current count is ${counter.getCount()}`);
