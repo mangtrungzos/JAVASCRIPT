923d0a20108089dd025684f902ecb521f477c63b
Johan - UTer <101387957+mangtrungzos@users.noreply.github.com>
2024-08-16 03:53:16 +0000

    16/8

---
+++ DOM/DOMNavigation/AddChangeHTML/index.js
@@ -0,0 +1,23 @@
+// Example <h1>
+// 1. Create the Element
+const newH1 = document.createElement("h1");
+
+// 2. Add Attributes / Properties
+newH1.textContent = "I like pizza!";
+newH1.id = "myh1";
+newH1.style.color = "tomato";
+newH1.style.textAlign = "center";
+
+// 3. Append Element to DOM
+// document.body.append(newH1);
+// document.body.prepend(newH1);
+// document.getElementById("box1").append(newH1);
+// document.getElementById("box1").prepend(newH1);
+
+// const box1 = document.getElementById("box3");
+// document.body.insertBefore(newH1, box1);
+
+const boxes = document.querySelectorAll(".box");
+document.body.insertBefore(newElement, boxes);
+
+// Remove HTML Element
