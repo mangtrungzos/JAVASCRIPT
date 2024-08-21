fc6838cfffa629235d2c491f6c5d6a93a01eaaa2
Johan - UTer <101387957+mangtrungzos@users.noreply.github.com>
2024-08-21 10:32:09 +0000

    21/8

--- RockPaperScissors/index.js
+++ RockPaperScissors/index.js
@@ -1,1 +1,52 @@
 // Rock Paper Scissors
+const choices = ["rock", "paper", "scissors"];
+const playerDisplay = document.querySelector("#playerDisplay");
+const computerDisplay = document.querySelector("#computerDisplay");
+const resultDisplay = document.querySelector("#resultDisplay");
+const playerScoreDisplay = document.querySelector("#playerScoreDisplay");
+const computerScoreDisplay = document.querySelector("#computerScoreDisplay");
+let playerScore = 0;
+let computerScore = 0;
+
+function playGame(playerChoice) {
+  const computerChoice = choices[Math.floor(Math.random() * 3)];
+  let result = "";
+
+  if (playerChoice === computerChoice) {
+    result = "IT'S A TIE!";
+  } else {
+    switch (playerChoice) {
+      case "rock":
+        result = computerChoice === "scissors" ? "YOU WIN!" : "YOU LOSE!";
+        break;
+      case "paper":
+        result = computerChoice === "rock" ? "YOU WIN!" : "YOU LOSE!";
+        break;
+      case "scissors":
+        result = computerChoice === "paper" ? "YOU WIN!" : "YOU LOSE!";
+        break;
+    }
+  }
+
+  playerDisplay.textContent = `PLAYER: ${playerChoice}`;
+  computerDisplay.textContent = `COMPUTER: ${computerChoice}`;
+  resultDisplay.textContent = result;
+
+  resultDisplay.classList.remove("greenText", "redText", "grayText");
+
+  switch (result) {
+    case "YOU WIN!":
+      resultDisplay.classList.add("greenText");
+      playerScore++;
+      playerScoreDisplay.textContent = playerScore;
+      break;
+    case "YOU LOSE!":
+      resultDisplay.classList.add("redText");
+      computerScore++;
+      computerScoreDisplay.textContent = computerScore;
+      break;
+    case "IT'S A TIE!":
+      resultDisplay.classList.add("grayText");
+      break;
+  }
+}
