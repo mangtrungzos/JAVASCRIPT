5d5702dffeef45919eec24519b422ed52311dbeb
Johan - UTer <101387957+mangtrungzos@users.noreply.github.com>
2024-08-21 11:14:15 +0000

    21/8

---
+++ callback/CallBackHell/index.js
@@ -0,0 +1,5 @@
+// CallBack Hell = Situation in JS where callbacks
+//                 are rested within other callbacks to the
+//                 degree where the code is difficult to read
+//                 Old pattern to handle asynchronous functions.
+//                 Use Promises + Async/Await to avoid Callback Hell.
