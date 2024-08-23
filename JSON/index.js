78eb164dfc0204e1fd26a00683afceb6406c5f7f
Johan - UTer <101387957+mangtrungzos@users.noreply.github.com>
2024-08-23 10:29:30 +0000

    23/8

--- JSON/index.js
+++ JSON/index.js
@@ -1,3 +1,10 @@
+// JSON = (JS Object Notation: ký tự) data-interchange format
+//        Used for exchanging data between a server and a web application
+//        JSON files { key: value } OR { value1, value2, value3 }
+
+//        JSON.stringify() = converts a JS object to a JSON string
+//        JSON.parse() = converts a JSON string to a JS object
+
 //        JSON.stringify() = converts a JS object to a JSON string
 const names = ["Vu", "Trong", "Sang"];
 const person = {
@@ -51,4 +58,3 @@ fetch("people.json")
   // response.json() return a promise
   .then((response) => response.json())
   .then((values) => values.forEach((value) => console.log(value.name)));
-  
