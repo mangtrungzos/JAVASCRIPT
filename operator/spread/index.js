236a031e22a43e77857fd93bfff8e66108580c6b
Johan - UTer <101387957+mangtrungzos@users.noreply.github.com>
2024-10-10 11:25:18 +0000

    10/10

--- operator/spread/index.js
+++ operator/spread/index.js
@@ -1,13 +1,13 @@
 // spread operator = ... allows an iterable such as an arr or string
 //                       to be expanded into separate elements
-//                       (unpacks the elements) 
+//                       (unpacks the elements)
 
+// Mở rộng thành các phần tử riêng lẻ
 // let numbers = [1, 2, 3, 4, 5];
 // let maximum = Math.max(...numbers);
 // let minimum = Math.min(...numbers);
 
 // console.log(minimum);
-
 
 // let username = "always write code";
 // let letters = [...username].join(" ");
@@ -20,3 +20,6 @@ let vegetables = ["carrot", "onion", "pea"];
 let foods = [...fruits, ...vegetables, "eggs", "milk"];
 
 console.log(foods);
+
+// const arr = [1, 2, 3];
+// console.log(...arr);  // 1 2 3 (mở rộng thành các phần tử riêng lẻ)
